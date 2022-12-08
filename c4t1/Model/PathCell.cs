9af33f16fe41ab17edce69d3d2cec8390fde5729
EZhitnikovich <ezhitnikovich@gmail.com>
2022-12-08 14:15:16 +0300

    change PathFinder

---
+++ c4t1/Model/PathCell.cs
@@ -0,0 +1,19 @@
+﻿namespace c4t1.Model;
+
+public class PathCell
+{
+    public Vector3Int Location { get; set; }
+
+    public double G { get; set; }
+    public double H { get; set; }
+    public double F { get; set; }
+
+    public bool Closed { get; set; }
+
+    public PathCell Parent { get; set; }
+
+    public PathCell(Vector3Int location)
+    {
+        Location = location;
+    }
+}
---
+++ c4t1/Model/Vector3Int.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c4t1.Model
+{
+	public struct Vector3Int
+	{
+		public Vector3Int(int x, int y, int z)
+		{
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public static bool operator ==(Vector3Int v1, Vector3Int v2) => v1.X == v2.X && v1.Y == v2.Y && v1.Z == v2.Z;
+
+        public static bool operator !=(Vector3Int v1, Vector3Int v2) => !(v1 == v2);
+    }
+}
